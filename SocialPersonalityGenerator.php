ce873233101d36b5a12a45542bf095c8cf6105a5
Rob Timmer <rob@robtimmer.com>
2016-09-10 15:52:28 +0200

    IBM Watson API implementation

--- SocialPersonalityGenerator.php
+++ SocialPersonalityGenerator.php
@@ -1,14 +1,18 @@
 <?php
 use Facebook\Facebook as Facebook;
 use Facebook\Exceptions\FacebookResponseException as FacebookResponseException;
-use FAcebook\Exceptions\FacebookSDKException as FacebookSDKException;
+use Facebook\Exceptions\FacebookSDKException as FacebookSDKException;
+use GuzzleHttp\Client as GuzzleHttpClient;
 
 class SocialPersonalityGenerator
 {
-
+    /**
+     * Facebook Application configuration
+     * @var array
+     */
     private $facebookAppConfig = array(
         'app_id' => '1769515346629314',
-        'app_secret' => '<INSERT APP SECRET>',
+        'app_secret' => '<INSERT-APP-SECRET>',
         'default_graph_version' => 'v2.7',
         'enable_beta_mode' => false,
         'http_client_handler' => null,
@@ -17,6 +21,22 @@ class SocialPersonalityGenerator
         'url_detection_handler' => null,
     );
 
+    /**
+     * IBM Watson API endpoint configuration
+     * @var array
+     */
+    private $IBMWatsonConfig = array(
+        'endpoint' => 'https://gateway.watsonplatform.net/personality-insights/api/v2/profile',
+        'auth' => array(
+            'username' => 'b0e950d0-4edb-4924-a489-7aa6073e7afc',
+            'password' => '<INSERT-WATSON-PASSWORD>'
+        )
+    );
+
+    /**
+     * Minimal content word count
+     * @var int
+     */
     private $contentWordCountMinimum = 200;
 
     /**
@@ -28,6 +48,12 @@ class SocialPersonalityGenerator
         return new Facebook($this->facebookAppConfig);
     }
 
+    /**
+     * Reads the Facebook posts content and returns it in a string
+     * @param $facebookApp
+     * @param $accessToken
+     * @return string
+     */
     public function getFacebookUserPosts($facebookApp, $accessToken)
     {
         try {
@@ -58,4 +84,35 @@ class SocialPersonalityGenerator
         return $contentString;
     }
 
+    /**
+     * Reads an generated profile from the IBM Watson API by given content
+     * @param $contentString
+     * @return string
+     */
+    public function getWatsonProfile($contentString)
+    {
+        $client = new GuzzleHttpClient();
+        //Open an new memory stream for writing and reading the Watson response attachments
+        $memoryWriteStream = fopen('php://memory', 'w');
+
+        //Make an request to the Watson API
+        $client->post($this->IBMWatsonConfig['endpoint'], [
+            'auth' => [
+                $this->IBMWatsonConfig['auth']['username'],
+                $this->IBMWatsonConfig['auth']['password']
+            ],
+            'headers' => [
+                'content-type' => 'text/plain',
+                'Content-Language' => 'en',
+                'Accept' => 'application/json',
+                'Accept-Language' => 'en'
+            ],
+            'body' => $contentString,
+            'verify' => false,
+            'save_to' => $memoryWriteStream
+        ]);
+
+        //Read the stream resource contents and return it
+        return stream_get_contents($memoryWriteStream);
+    }
 }
--- index.php
+++ index.php
@@ -4,12 +4,17 @@ require 'vendor/autoload.php';
 //Include class
 include 'SocialPersonalityGenerator.php';
 
+$facebookUserAccessToken = '<INSERT-USER-ACCESS-TOKEN>';
+
 $socialPersonalityGenerator = new SocialPersonalityGenerator();
 //Initialize Facebook application
 $facebookApp = $socialPersonalityGenerator->initializeFacebookApp();
-
-
-$userAccessToken = '<INSERT ACCESS TOKEN>';
 //Gather the needed user posts
-echo '<pre>';
-print_r($socialPersonalityGenerator->getFacebookUserPosts($facebookApp, $userAccessToken));
+$contentString = $socialPersonalityGenerator->getFacebookUserPosts($facebookApp, $facebookUserAccessToken);
+//Get Watson profile
+$watsonProfile = $socialPersonalityGenerator->getWatsonProfile($contentString);
+
+//JSON response output
+header('Content-Type: application/json');
+echo $watsonProfile;
+die();
